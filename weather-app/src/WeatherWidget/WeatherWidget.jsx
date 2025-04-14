77c3ce2be9d0703c7788527f2d8edb75fdfd0b95
Nicolas Bourahoui <nicolas.bourahoui@gmail.com>
2025-04-14 11:04:51 +0200

    Ajout du style dans weatherwidget.css et import de celui ci dans le jsx

--- weather-app/src/WeatherWidget/WeatherWidget.jsx
+++ weather-app/src/WeatherWidget/WeatherWidget.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import styles from "./WeatherWidget.css";
 
 
 // Clé API OpenWeatherMap
@@ -72,3 +73,5 @@ const WeatherWidget = ({city, lang = "fr", units = "metrics" }) => {
     </div>
   );
 };
+
+export default WeatherWidget;
