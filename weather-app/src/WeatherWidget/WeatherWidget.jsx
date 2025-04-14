7668e8748cb41d08eac8bf537e983485b8fc153a
Nicolas Bourahoui <nicolas.bourahoui@gmail.com>
2025-04-14 15:32:56 +0200

    Modification de plusieurs fichier pour améliorer le style du widget

--- weather-app/src/WeatherWidget/WeatherWidget.jsx
+++ weather-app/src/WeatherWidget/WeatherWidget.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import styles from "./WeatherWidget.css";
+import styles from "./WeatherWidget.module.css";
 
 
 // Clé API OpenWeatherMap
